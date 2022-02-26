57f5eb1b7e1541e2d5ee4e065860037a1c5660f3
Mingwei Zhang <mingwei@mwzhang.com>
2022-02-26 10:25:47 -0800

    load readme file as pypi description

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 import setuptools
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setuptools.setup(
     name='pybgpkit',
-    version='0.0.2',
+    version='0.0.2.post1',
     description='BGPKIT tools Python bindings',
     url='https://github.com/bgpkit/pybgpkit',
     author='Mingwei Zhang',
     author_email='mingwei@bgpkit.com',
     packages=setuptools.find_packages(),
     include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         # available on pip
         'pybgpkit-parser==0.0.1',
