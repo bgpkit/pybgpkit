33fa05d0c5ed1ab5aa9379ef7414c9853f55f23d
Mingwei Zhang <mingwei@mwzhang.com>
2023-04-17 14:14:15 -0700

    bump versions

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import bgpkit
 parser = bgpkit.Parser(url="https://spaces.bgpkit.org/parser/update-example",
-                       filters={"peer_ips": "185.1.8.65, 2001:7f8:73:0:3:fa4:0:1"})
+                       filters={"peer_ips": "185.1.8.65, 2001:7f8:73:0:3:fa4:0:1"},
+                       cache_dir="cache"
+                       )
 elems = parser.parse_all()
 assert len(elems) == 4227
 import json
