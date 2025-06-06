feeaa9f632aac5c8184a01078cefa0243e7de482
Mingwei Zhang <mingwei@mwzhang.com>
2025-06-06 10:10:29 -0700

    bump version to 0.6.0

--- setup.py
+++ setup.py
@@ -2,24 +2,25 @@ import setuptools
 
 # read the contents of your README file
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
-    name='pybgpkit',
-    version='0.5.2',
-    description='BGPKIT tools Python bindings',
-    url='https://github.com/bgpkit/pybgpkit',
-    author='Mingwei Zhang',
-    author_email='mingwei@bgpkit.com',
+    name="pybgpkit",
+    version="0.6.0",
+    description="BGPKIT tools Python bindings",
+    url="https://github.com/bgpkit/pybgpkit",
+    author="Mingwei Zhang",
+    author_email="mingwei@bgpkit.com",
     packages=setuptools.find_packages(),
     include_package_data=True,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     install_requires=[
         # available on pip
-        'dataclasses_json',
-        'pybgpkit-parser==0.5.2',
-        'requests',
-    ]
+        "dataclasses_json",
+        "pybgpkit-parser==0.6.0",
+        "requests",
+    ],
 )
